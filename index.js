223d2f20f65ba6f2264336927eb0a4724c29ccd2
Nezeri <ichigvintsev@icloud.com>
2026-03-03 18:08:30 +0500

    1-3

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ function getFiles() {
 
 function getComments() {
     let result = [];
+    let i = 0;
     for (const file of files) {
-        for (const line of file.lines) {
-            if (line.startsWith('// TODO ')) {
-                result.push(line);
+        for (const line of file.split('\n')) {
+            const idx = line.indexOf('// TODO ')
+            if (idx != -1 && line.indexOf('indexOf') == -1) {            
+                //console.log(line)
+                result.push(line.slice(idx + 8));
             }
         }
     }
@@ -24,12 +27,33 @@ function getComments() {
     return result;
 }
 
+function parseLine(line){
+    // [string] => [userInfo, Date, text, importance]
+    const meta = line.split(';');
+    return [meta[0], new Date(meta[1]), meta[2], line.match(/!/g)];
+}
+
 function processCommand(command) {
+    const comments = getComments();
+    const predprocessed = comments.map(parseLine); 
+    //console.log(predprocessed[0]);
+
+    //getComments();
     switch (command) {
         case 'show':
+            for (todo of comments){
+                console.log(todo);
+            }
         case 'important':
+            for (todo of predprocessed){
+                if (todo[3] !== null){
+                    console.log(`${meta[0]}; ${formatDate(meta[1])}; ${meta[2]};`);
+                }
+            }
         case 'user':
-        
+            for (todo of)
+        case 'sort':
+            //predprocessed.sort((a, b) => b[3] >= a[3])
         case 'exit':
             process.exit(0);
             break;
@@ -37,6 +61,12 @@ function processCommand(command) {
             console.log('wrong command');
             break;
     }
+}
+
+function formatDate (date) {
+    const d = date.getDate().toString().padStart(2, '0');
+    const m = (date.getMonth() + 1).toString().padStart(2, '0');
+    return `${d}-${m}-${date.getFullYear()}`
 }
 
 // TODO you can do it!
